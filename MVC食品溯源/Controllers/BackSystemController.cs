687cde8572af0c899cffe9d1fa285b9511ca7d9f
mscyan <yanchao00001@hotmail.com>
2017-11-29 17:29:36 +0800

    后台管理

--- MVC食品溯源/Controllers/BackSystemController.cs
+++ MVC食品溯源/Controllers/BackSystemController.cs
@@ -14,7 +14,32 @@ namespace MVC食品溯源.Controllers
             return View();
         }
 
+		//角色管理页面
 		public ActionResult Roles()
+		{
+			return View();
+		}
+
+		//用户管理页面
+		public ActionResult Users()
+		{
+			return View();
+		}
+
+		//申请回复页面
+		public ActionResult Replys()
+		{
+			return View();
+		}
+
+		//厂商信息页面
+		public ActionResult Companys()
+		{
+			return View();
+		}
+
+		//菜单显示页面
+		public ActionResult Menus()
 		{
 			return View();
 		}
